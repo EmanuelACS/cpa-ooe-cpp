1ad2c57859eba2d5b047cd0efc336a3d7473b604
Emanuel Dobra <ema.dobra@gmail.com>
2021-09-23 17:17:43 -0400

    Added Lab2

---
+++ Labs/Lab2/Classroll.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+/**
+ * Name: Emanuel Dobra
+ * Specification file for ClassRoll
+*/
+
+struct StudentRec {
+    string studentName;
+    int assignmentTotal;
+    int testTotal;
+    char letterGrade;
+};
+
+
+const int SIZE = 25;
+
+class ClassRoll {
+    public: 
+        void showGrade(string /*studentName*/) const; // Displays a student's grade
+        int getNoStudents() const { return noStudents }; // Returns the amount of students in array
+        string getCourseName() const { return courseName }; // Returns name of the course
+        void changeGrade(string /*studentName*/, char /*letterGrade*/); // Change a student's grade
+        void writeStudents(ostream%) const; // Write all of the students to an output file or screen 
+    } 
+
+    private:
+        string courseName;
+        int maxStudents;
+        int noStudents;
+        StudentRec students[];
+        int searchStudent(string /*studentName*/) const { return studentIndex }; // Returns the index of the student in the students array
+};
+
