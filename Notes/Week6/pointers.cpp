614c10ea9ee487f300af674de627b9a142fba057
Emanuel Dobra <ema.dobra@gmail.com>
2021-10-13 12:38:29 -0400

    Updated week6 notes

--- Notes/Week6/pointers.cpp
+++ Notes/Week6/pointers.cpp
@@ -23,5 +23,32 @@ int main() {
     // first thing is to make sure we have enough memory when declaring heap memory
     assert(heapIntPtr != NULL); // ensures memory was allocated
     cout << "heapIntPtr has a value of " << *heapIntPtr << endl;
+    delete heapIntPtr; // frees up run time memory (which was allocated on the heap)
 
+    // pointer of different types on the heap
+    char* charPtr = new char('z');
+    assert(charPtr); // also works
+    cout << "charPtr has a value of " << *charPtr << endl;
+    delete charPtr; 
+
+    string*  descPtr = new string;
+    assert(descPtr != 0); // also works
+    *descPtr = "testing";
+    cout << "descPtr has a value of " << *descPtr << endl;  
+    delete descPtr;
+
+    int* arrPtr = new int[5]; // create array on the heap of size 5
+    assert(arrPtr);
+    int* pointToArrPtr = arrPtr; // points to arrPtr[0]
+    arrPtr[0] = 49;
+    arrPtr[1] = 99;
+    cout << arrPtr[0] << " - " << arrPtr[1] << endl; 
+    pointToArrPtr++; // points to arrPtr[1] now
+    cout << *pointToArrPtr << endl; // 
+    delete []arrPtr; // deallocate array memory
+    // deletes all elements in the array
+    // There is no need to delete pointToArrPtr since the memory of arrPtr is already deleted.
+    // delete []pointToArrPtr; 
+    
+    
 }
