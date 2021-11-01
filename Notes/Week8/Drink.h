e6d78a71570457389ed3727baf3b41b765ebec0c
Emanuel Dobra <ema.dobra@gmail.com>
2021-11-01 14:14:46 -0400

    Finished Drink.h

---
+++ Notes/Week8/Drink.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cassert> 
+using namespace std;
+
+class Drink {
+    public:
+        Drink();
+        Drink(int /*ounces*/, string /*type*/);
+        int getQty() const { return qty; }
+        string getTypeOfDrink() const { return *typeOfDrink; }
+        void setQty(int /*ounces*/);
+        void setTypeOfDrink(string /*type*/); 
+        void show() const;
+    private:
+        int qty;
+        string* typeOfDrink;
+};
