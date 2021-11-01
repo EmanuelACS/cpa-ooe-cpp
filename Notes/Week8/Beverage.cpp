931e9d3601a2ce28b40645e682823f913c62b79a
Emanuel Dobra <ema.dobra@gmail.com>
2021-11-01 14:51:34 -0400

    Finished implemenation files, added ClientCode

--- Notes/Week8/Beverage.cpp
+++ Notes/Week8/Beverage.cpp
@@ -2,3 +2,20 @@
 
 // implementation file for Beverage class
 
+Beverage::Beverage() {
+    cout << "Called Default Beverage Constructor!\n";
+    mix = new string("no mix");
+    assert(mix!=NULL);
+    //qty = 0
+}
+
+Beverage::Beverage(int newQty, string newDrink, string newMix) {
+    cout << "Beverage non-default constructor called!\n";
+    mix = new string; // only assign new memory once in the constructor,
+    assert(mix!=0);
+    setMix(newMix);
+}
+
+void Beverage::setMix(string newMix) {
+    *mix = (newMix.length() > 0)?newMix:"no mix";
+}
--- Notes/Week8/Beverage.h
+++ Notes/Week8/Beverage.h
@@ -8,7 +8,7 @@ class Beverage : public Drink {
         Beverage(int /*qty*/, string /*typeOfDrink*/, string /*mix*/);
         string getMix() const { return *mix; };
         void setMix(string /*newMix*/);
-        void show() const;
+        //void show() const;
     private: 
         string* mix;            
 };
