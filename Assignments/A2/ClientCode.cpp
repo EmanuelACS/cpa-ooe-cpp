cf3876a36788ec6ac3630ad62ee4e36f1ff5889e
Emanuel Dobra <ema.dobra@gmail.com>
2021-11-17 21:19:38 -0500

    Added A2, reorganized A1 into a new directory

---
+++ Assignments/A2/ClientCode.cpp
@@ -0,0 +1,32 @@
+#include "Package.h"
+
+int main() {
+    Package myPackage("Home", "Work");
+    Letter myLetter(4, "Home3", "FL");
+    Box myBox(24, "Home2", "NY");
+
+    Package *test = new Package();
+    
+
+    cout << "Pkg Source: " << myPackage.getSource() << endl
+        << "Pkg Destination: " << myPackage.getDestination()
+        << endl;
+    
+    cout << "Letter Pages: " << myLetter.getnoPages() << endl
+        << "Letter Source: " << myLetter.getSource() << endl
+        << "Letter Destination: " << myLetter.getDestination()
+        << endl;
+
+    cout << "Box Weight: " << myBox.getWeight() << endl
+        << "Box Source: " << myBox.getSource() << endl
+        << "Box Destination: " << myBox.getDestination()
+        << endl;
+
+    Package *myPackage2 = new Package("@home", "@work");
+    Letter *myLetter2 = new Letter(99, "@home3", "@FL");
+    Box *myBox2 = new Box(17, "home2", "NY");
+
+    cout << "\nDefault package cost: " << myPackage2->computeShipCost() << endl;
+    cout << "Computing letter cost: " << myLetter2->computeShipCost() << endl;
+    cout << "Computing  box cost: " << myBox2->computeShipCost() << endl;
+}
---
+++ Assignments/A2/Package.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+using namespace std;
+
+// Emanuel Dobra
+// 2021-11-03
+// spec file for Package class
+
+class Package {
+    public:
+        Package();
+        Package(string /*source*/, string /*destination*/);
+        ~Package();
+        virtual double computeShipCost() const { return 0.0; }
+        string getSource() const { return *source; }
+        string getDestination() const { return *destination; }
+    protected:
+        string* source;
+        string* destination;
+};
+
+class Box : public Package {
+    public:
+        Box();
+        Box(int /*weight*/, string /*source*/, string /*destination*/);
+        double computeShipCost() const;
+        int getWeight() const { return weight; }
+    private:
+        int weight;
+};
+
+class Letter : public Package {
+    public:
+        Letter();  
+        Letter(int /*noPages*/, string /*source*/, string /*destination*/);
+        double computeShipCost() const;
+        int getnoPages() { return noPages; }
+    private:
+        int noPages; 
+};
